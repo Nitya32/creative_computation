763920ce45dc525c562c1397b025358c479e2975
nitya32 <26nitya02@gmail.com>
2019-12-03 18:56:01 -0500

    latest
    latest

---
+++ script/ex4-2.js
@@ -0,0 +1,7 @@
+$("#show_btn").click(function(){$("#show_target").show();});
+
+$("#fadeIn_btn").click(function(){$("#fadeIn_target").fadeIn(1000);});
+
+$("#fadeOut_btn").click(function(){$("#fadeOut_target").fadeOut(1000);});
+
+$("slide_btn").click(function(){$("#slide_target").slideUp(200);});
